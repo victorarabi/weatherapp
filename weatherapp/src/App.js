c11ba5d44e666f1b01a9be9175ebda68f48e96df
Victor Augusto Arabi Fernandes de Oliveira <victorarabi@hotmail.com>
2023-03-12 15:50:10 -0400

    Home page and App.js progress - no styling

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,8 +1,23 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
 import './App.scss';
 
 export default function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
---
+++ weatherapp/src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import './Home.scss';
+
+//Home page
+export default function Home() {
+  return (
+    <section className="home">
+      <p className="home__txt">Welcome to our weather app</p>
+    </section>
+  );
+}
