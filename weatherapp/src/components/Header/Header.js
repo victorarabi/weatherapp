e4468e6b99dc540cc2d02619fd1be20fb1702048
Victor Augusto Arabi Fernandes de Oliveira <victorarabi@hotmail.com>
2023-03-12 15:37:23 -0400

    Header component boilerplate html - no styling

---
+++ weatherapp/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import './Header.scss';
+
+//Header component
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Weather App</h1>
+    </header>
+  );
+}
