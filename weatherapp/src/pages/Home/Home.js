95a6f51ae2ea4d7bd7df95901dcc1dfd24b2fe18
Victor Augusto Arabi Fernandes de Oliveira <victorarabi@hotmail.com>
2023-03-12 16:21:50 -0400

    basic styling for footer component

--- weatherapp/src/pages/Home/Home.js
+++ weatherapp/src/pages/Home/Home.js
@@ -1,10 +1,13 @@
+import ContentContainer from '../../components/ContentContainer/ContentContainer';
 import './Home.scss';
 
 //Home page
 export default function Home() {
   return (
-    <section className="home">
-      <p className="home__txt">Welcome to our weather app</p>
-    </section>
+    <ContentContainer background="main">
+      <section className="home">
+        <p className="home__txt">Welcome to our weather app</p>
+      </section>
+    </ContentContainer>
   );
 }
