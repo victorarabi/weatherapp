87ac297a9960f2331193d27f1c5982be4def80f9
Victor Augusto Arabi Fernandes de Oliveira <victorarabi@hotmail.com>
2023-03-13 16:09:09 -0400

    created boilerplate for WeatherData page with basic state/url management - no styling

---
+++ weatherapp/src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import './NotFound.scss';
+
+//404 Not found component
+export default function NotFound() {
+  return (
+    <article className="not-found">
+      <h1>404 - Not Found</h1>
+      <p className="not-found_txt">Please verify addess.</p>
+    </article>
+  );
+}
--- weatherapp/src/pages/WeatherData/WeatherData.js
+++ weatherapp/src/pages/WeatherData/WeatherData.js
@@ -1,10 +1,31 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import NotFound from '../../components/NotFound/NotFound';
 import ContentContainer from '../../components/ContentContainer/ContentContainer';
 import './WeatherData.scss';
 
 //Weather Data component
 export default function WeatherData() {
+  const [isLoading, setIsLoading] = useState(false);
   const { location, lat, lng } = useParams();
+  //verifies if route parameters aren't empty and return NotFound if empty
+  if (!location || !lat || !lng) {
+    return (
+      <ContentContainer background="main">
+        <section className="weather-data">
+          <NotFound />;
+        </section>
+      </ContentContainer>
+    );
+  } else if (isLoading) {
+    return (
+      <ContentContainer background="main">
+        <section className="weather-data">
+          <h1 className="weather-data__loading">Loading data</h1>
+        </section>
+      </ContentContainer>
+    );
+  }
   return (
     <ContentContainer background="main">
       <section className="weather-data">
